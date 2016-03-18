0a797402f995c89482cdfdf4e39b5bb43c728598
AngeLo Biscola <angelo-tec@hotmail.com>
2016-03-18 11:28:15 -0300

    Update - Application Investment
    Upgrade seed Start

--- database/seeds/StartTableSeeder.php
+++ database/seeds/StartTableSeeder.php
@@ -20,7 +20,7 @@ class StartTableSeeder extends Seeder
 
         Company::create(['id' => 1, 'name' => 'New name', 'company_name' => 'New company name']);
         Admin::create(['name' => 'Admin', 'email' => 'admin@admin.com', 'password' => '$2y$10$r9q9YCj9rBkVjI093fKVueh8IZd8L8nWXGwyBS63BflWeDQ0nmjIe']);
-        Collaborator::create(['name' => 'Funcionario', 'email' => 'fund@afun.com', 'password' => '$2y$10$mfCZ3wwYpTuTUiU3jKonjeU.wj/eJhAu1ll08RBN6UQJLUGAnu9wq']);
+        Collaborator::create(['name' => 'Funcionario', 'email' => 'fun@fun.com', 'password' => '$2y$10$an.IIRbFy5so4/kWg4cm6eYZnBx7.suBpBqQfuJ99GvKbXm3aCtWK']);
 
     }
 }
