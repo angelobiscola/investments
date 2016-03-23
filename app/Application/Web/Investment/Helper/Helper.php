473cbe390ec830d8304999a6fcc17bd1a284d89b
AngeLo Biscola <angelo-tec@hotmail.com>
2016-03-23 10:49:29 -0300

    Update - Application Investment
    Helper

---
+++ app/Application/Web/Investment/Helper/Helper.php
@@ -0,0 +1,70 @@
+<?php
+
+if (! function_exists('jurosSimples')) {
+
+    function jurosSimples($value, $rate, $days, $date)
+    {
+        $formatDate = explode('-',$date);
+
+        $date    = \Carbon\Carbon::create($formatDate[0],$formatDate[1],$formatDate[2]);
+        $oldDate = \Carbon\Carbon::create($formatDate[0],$formatDate[1],$formatDate[2]);
+
+        $parcels     = $days/30;
+        $rate        = $rate / 100;
+
+        $daily_rate  = pow(1 + $rate, 1/30) - 1;
+
+        //dd($daily_rate);
+
+        //first date and firstParcel
+        $firstDate       = $date->addDay(1)->addMonth(1)->firstOfMonth();
+        $diffFirstDate   = $oldDate->diffInDays($firstDate);
+
+        $firstParcel     = ($value * $daily_rate) * $diffFirstDate;
+
+        //dd(($value * $daily_rate) * 360 / $parcels);
+
+        $total        = $value * (1 + $rate * ($parcels));
+        $interest     = $total - $value;
+        $valueParcels = ($interest / $parcels);
+
+        $details[] = ['date' => $firstDate->toDateString(), 'value' =>$firstParcel,'diff' => $diffFirstDate];
+
+        for($i =0; $i < $parcels-2; $i++)
+        {
+            $details[] = ['date' => $firstDate->addMonth()->toDateString(), 'value' =>$valueParcels, 'diff' => ' ' ];
+        }
+
+        $lastDate      = $oldDate->addDays($days+$diffFirstDate);
+        $diffLastDate  = $firstDate->addMonth()->diffInDays($lastDate);
+        $lastParcel    = ($value * $daily_rate) * $diffLastDate;
+
+        //$teste = \Carbon\Carbon::now()->addMonth(1)->firstOfMonth();
+        //dd($teste->diffInDays($last));
+
+        $details[] = ['date' => $lastDate->toDateString(), 'value' =>$lastParcel, 'diff' => $diffLastDate];
+
+        $total    = ($total - ($valueParcels*2)) + ($firstParcel+$lastParcel);
+        $interest = $total - $value;
+
+        return ['total' => $total, 'value_parcels' => $valueParcels, 'interest' =>$interest,'rate'=> $rate*100, 'parcels' =>$parcels, 'details' => $details];
+    }
+}
+
+
+
+if (! function_exists('jurosComposto')) {
+
+    function jurosComposto($value, $rate, $parcels) {
+
+        $rate = $rate / 100;
+        $total = $value * pow((1 + $rate), $parcels);
+        $interest  = $total - $value;
+
+        return ['total' => $total, 'interest' =>$interest,'rate'=> $rate*100, 'date' =>\Carbon\Carbon::now()->addDay(360)];
+    }
+}
+
+
+
+
