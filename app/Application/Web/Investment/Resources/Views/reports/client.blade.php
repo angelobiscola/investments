d1ccf9f1c9276c56ecc230478b23aa60a62c4af4
robsonneves <robsonnevesrodrigues@yahoo.com.br>
2016-04-05 16:52:43 -0300

    Fix Reports Client

--- app/Application/Web/Investment/Resources/Views/reports/client.blade.php
+++ app/Application/Web/Investment/Resources/Views/reports/client.blade.php
@@ -88,6 +88,10 @@
             width: 90% !important;
         }
 
+        .page-break {
+            page-break-after: always;
+        }
+
     </style>
 </head>
 
@@ -104,41 +108,9 @@
         </strong>
     </div>
 
-    <div class="content">
-        <div class="title"> Dados Da conta na CAIXA não tem </div>
-
-        <div class="field">
-            <div class="size">
-                <div class="fild-title"> Cod Agência </div>
-                <div class="fild-body"> 123 </div>
-            </div>
-            <div class="size">
-                <div class="fild-title"> Nome Agência </div>
-                <div class="fild-body"> Agência </div>
-            </div>
-            <div class="size">
-                <div class="fild-title"> Conta Conjunta </div>
-                <div class="fild-body"> Sim / Nao  </div>
-            </div>
-            <div class="size">
-                <div class="fild-title"> Cod Op. </div>
-                <div class="fild-body"> 45 </div>
-            </div>
-            <div class="size">
-                <div class="fild-title"> N° Conta Corrente </div>
-                <div class="fild-body"> 98765 </div>
-            </div>
-            <div class="size">
-                <div class="fild-title"> N° DV </div>
-                <div class="fild-body"> 1 </div>
-            </div>
-
-        </div>
-    </div>
-
     <br><br>
 
-    @if($client->type == 'f')
+   @if($client->type == 'f')
 
         <div class="content">
             <div class="title"><strong> Dados Pessoais </strong></div>
@@ -212,16 +184,14 @@
         <div class="content">
             <div class="title"> Estado Civil </div>
             <div class="list">
-                <ol>
+                <ul>
                     <li> {!! $client->Physical->marital_status  !!} </li>
-                </ol>
+                </ul>
             </div>
         </div>
 
-        <br><br>
-
         <div class="content">
-            <div class="title"> Endereço Residencial</div>
+            <div class="title"> Endereço </div>
 
             <div class="field">
                 <div class="size">
@@ -259,12 +229,98 @@
             </div>
         </div>
 
-    @else
+       <br><br>
 
-        @foreach($client->Legal->Representatives as $c)
+    @else
+        <div class="content">
+            <div class="title"><strong> Dados Da Empresa </strong></div>
+            <div class="field">
+                <div class="size col-50">
+                    <div class="fild-title"> CNPJ </div>
+                    <div class="fild-body"> {!! $client->Legal->present()->cnpjLegal !!} </div>
+                </div>
+            </div>
+        </div>
+
+        <br><br>
+
+        <div class="content">
+            <div class="field">
+                <div class="size col-90">
+                    <div class="fild-title"> Nome </div>
+                    <div class="fild-body"> {!! $client->Legal->company_name !!} </div>
+                </div>
+            </div>
+        </div>
+
+        <br><br>
+
+        <div class="content">
+            <div class="field">
+                <div class="size">
+                    <div class="fild-title"> Telefone </div>
+                    <div class="fild-body"> {!! $client->phone !!} </div>
+                </div>
+                <div class="size">
+                    <div class="fild-title"> E-mail </div>
+                    <div class="fild-body"> {!! $client->Legal->email !!} </div>
+                </div>
+            </div>
+        </div>
+
+        <br><br>
+
+        <div class="content">
+            <div class="title"> Endereço </div>
+
+            <div class="field">
+                <div class="size">
+                    <div class="fild-title"> CEP </div>
+                    <div class="fild-body"> {!! $client->Location->zip_code !!} </div>
+                </div>
+
+                <div class="size">
+                    <div class="fild-title"> Rua / Avenida / Alameda / Travessa </div>
+                    <div class="fild-body"> {!! $client->Location->address !!} </div>
+                </div>
+                <div class="size">
+                    <div class="fild-title"> Numero </div>
+                    <div class="fild-body"> {!! $client->Location->number !!} </div>
+                </div>
+                <div class="size">
+                    <div class="fild-title"> Complemento </div>
+                    <div class="fild-body"> casa nao tem </div>
+                </div>
+                <div class="size">
+                    <div class="fild-title"> Bairro </div>
+                    <div class="fild-body"> {!! $client->Location->district !!} </div>
+                </div>
+            </div>
+        </div>
+
+        <br><br>
+
+        <div class="content">
+            <div class="field">
+                <div class="size">
+                    <div class="fild-title"> UF </div>
+                    <div class="fild-body"> {!! $client->Location->state_abbr !!} </div>
+                </div>
+            </div>
+            <div class="field">
+                <div class="size">
+                    <div class="fild-title"> Municipio </div>
+                    <div class="fild-body"> {!! $client->Location->city !!} </div>
+                </div>
+            </div>
+        </div>
+
+        <br><br>
+
+        @foreach($client->Legal->Representatives as $k=>$c)
 
             <div class="content">
-                <div class="title"><strong> Dados Pessoais </strong></div>
+                <div class="title"><strong> Dados Do Representante </strong></div>
                 <div class="field">
                     <div class="size col-50">
                         <div class="fild-title"> CPF do Cliente </div>
@@ -335,13 +391,11 @@
             <div class="content">
                 <div class="title"> Estado Civil </div>
                 <div class="list">
-                    <ol>
+                    <ul>
                         <li> {!! $c->client->Physical->marital_status  !!} </li>
-                    </ol>
+                    </ul>
                 </div>
             </div>
-
-            <br><br>
 
             <div class="content">
                 <div class="title"> Endereço Residencial</div>
@@ -371,6 +425,13 @@
                         <div class="fild-title"> Bairro </div>
                         <div class="fild-body"> {!! $c->client->Location->district !!} </div>
                     </div>
+                </div>
+            </div>
+
+            <br><br>
+
+            <div class="content">
+                <div class="field">
                     <div class="size">
                         <div class="fild-title"> UF </div>
                         <div class="fild-body"> {!! $c->client->Location->state_abbr !!} </div>
@@ -384,17 +445,56 @@
 
             <br><br>
 
+            @if($client->Legal->Representatives->count() == 2 and $k == 0)
+                <div class="page-break"></div>
+            @endif
+
         @endforeach
 
     @endif
+
+    <div class="content">
+        <div class="title"><strong> Dados Da Conta </strong></div>
+
+        <div class="field">
+            <div class="size">
+                <div class="fild-title"> Cod Agência </div>
+                <div class="fild-body"> 123 </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> Nome Agência </div>
+                <div class="fild-body"> Agência </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> Conta Conjunta </div>
+                <div class="fild-body"> Sim / Nao  </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> Cod Op. </div>
+                <div class="fild-body"> 45 </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> N° Conta Corrente </div>
+                <div class="fild-body"> 98765 </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> N° DV </div>
+                <div class="fild-body"> 1 </div>
+            </div>
+        </div>
+    </div>
+
+    <br><br>
+    <br><br>
 
 </div>
 </body>
 
 <script>
 
-        window.print();
+   window.print();
 
 </script>
+
 
 </html>
