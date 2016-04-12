5cdc9d5fb22e6a08281c52fcda5d1a057c1ae31f
robsonneves <robsonnevesrodrigues@yahoo.com.br>
2016-04-12 16:48:24 -0300

    Fix Bank inputs

--- app/Application/Web/Investment/Resources/Views/reports/_bank.blade.php
+++ app/Application/Web/Investment/Resources/Views/reports/_bank.blade.php
@@ -1,33 +1,34 @@
 <div class="content">
-    <div class="title"> Dados Da Conta </div>
 
     @foreach($client->Banks as $bank)
-    <div class="field">
-        <div class="size">
-            <div class="fild-title"> Nome Agência </div>
-            <div class="fild-body"> {!! $bank->name !!} </div>
+
+        <div class="title"> Dados Da Conta </div>
+
+        <div class="field">
+            <div class="size">
+                <div class="fild-title"> Nome Agência </div>
+                <div class="fild-body"> {!! $bank->name !!} </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> Local </div>
+                <div class="fild-body"> {!! $bank->city !!} </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> Cod Agência </div>
+                <div class="fild-body"> {!! $bank->agency !!} </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> Tipo. </div>
+                <div class="fild-body"> {!! $bank->type == 'c' ? 'Corrente' : 'Poupança' !!} </div>
+            </div>
+            <div class="size">
+                <div class="fild-title"> N° Conta </div>
+                <div class="fild-body"> {!! $bank->account !!} </div>
+            </div>
         </div>
-        <div class="size">
-            <div class="fild-title"> Local </div>
-            <div class="fild-body"> {!! $bank->city !!} </div>
-        </div>
-        <div class="size">
-            <div class="fild-title"> Cod Agência </div>
-            <div class="fild-body"> {!! $bank->agency !!} </div>
-        </div>
-        <div class="size">
-            <div class="fild-title"> Tipo. </div>
-            <div class="fild-body"> {!! $bank->type == 'c' ? 'Corrente' : 'Poupança' !!} </div>
-        </div>
-        <div class="size">
-            <div class="fild-title"> N° Conta </div>
-            <div class="fild-body"> {!! $bank->account !!} </div>
-        </div>
-    </div>
+        <br><br><br>
 
     @endforeach
 
 </div>
 
-<br><br>
-<br><br>
